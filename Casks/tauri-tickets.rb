f89406ee138b2469eb0f3a6842b85831aaa4eab1
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-10-28 15:25:55 -0600

    Update tauri-tickets.rb

--- Casks/tauri-tickets.rb
+++ Casks/tauri-tickets.rb
@@ -2,7 +2,7 @@ cask "tauri-tickets" do
     version "0.6.4"
     sha256 "336f40cbac8bb09bf291d2624da79d014788086266b876d4ca459606bbc709a5"
   
-    url "https://github.com/daniel-le97/tauri-ticket-manager/releases/tag/v0.6.4"
+    url "https://github.com/daniel-le97/tauri-ticket-manager/releases/download/v#{version}/tauri-tickets_x64.app.tar.gz"
     name "tauri-tickets"
     desc "a ticket/note manager made with tauri"
     homepage "https://github.com/daniel-le97/tauri-ticket-manager"
